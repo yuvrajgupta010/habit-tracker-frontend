746af87321b679dfa7fcc6eb389c2d9d8cd7b6a9
Yuvraj Gupta <Yuvrajgupta010@gmail.com>
2024-10-21 16:25:06 +0530

    req server path edit

--- src/services/restfulUrls.js
+++ src/services/restfulUrls.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://api.habit-tracker.yuvrajgupta.in/";
-export const BASE_URL = "http://localhost:8080/";
+export const BASE_URL = "https://api.habit-tracker.yuvrajgupta.in/";
+// export const BASE_URL = "http://localhost:8080/";
 
 export const RestfulUrls = {
   SIGN_UP: "/auth/signup",
