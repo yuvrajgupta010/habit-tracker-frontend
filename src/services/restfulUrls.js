aca0f2d460ab7c14afb27487e0e47defd5be0009
Yuvraj Gupta <yuvrajgupta036@gmail.com>
2025-06-17 16:30:50 +0530

    update in base url

--- src/services/restfulUrls.js
+++ src/services/restfulUrls.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://api.habit-tracker.yuvrajgupta.in/";
+export const BASE_URL = "https://api-habit-tracker.yuvrajgupta.in/";
 // export const BASE_URL = "http://localhost:8080/";
 
 export const RestfulUrls = {
